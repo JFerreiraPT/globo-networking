034ca27e653426cc112954f0d76eba4ea45d2fd2
João Ferreira <j.ferreira@xgeeks.com>
2025-08-28 12:12:48 +0100

    add workspace tag and remove defaults

--- resources.tf
+++ resources.tf
@@ -18,7 +18,8 @@ data "aws_availability_zones" "available" {}
 locals {
   common_tags = {
     Environment  = var.environment
-    billing_code = var.billing_code
+    Billing_code = var.billing_code
+    Workspace    = terraform.workspace
   }
 }
 
